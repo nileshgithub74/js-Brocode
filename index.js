2eee640fc41042f996681d636575f1d01518e8d1
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-08-13 15:13:41 +0000

    JavaScript

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log(`this is nilesh kumar`);
+console.log("i liked briyani");
+
+
+// window.alert(`this is  an alert`);
+// window.alert("this is nilesh");
+
+
+// this is comment
+
+/*
+
+this is multi- line comment
+
+*/
+
+
+document.getElementById("myh1").textContent = "heey this is nilesh";
+
+document.getElementById("myP").textContent = "i like briyani.";
+
+
+
+// varaiable in javascript
