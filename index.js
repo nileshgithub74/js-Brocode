052a06068e549537839718de2fe128e83c0b25a2
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-08-14 02:00:48 +0000

    javaScript

--- index.js
+++ index.js
@@ -15,10 +15,49 @@ this is multi- line comment
 */
 
 
-document.getElementById("myh1").textContent = "heey this is nilesh";
+// document.getElementById("myh1").textContent = "heey this is nilesh";
 
-document.getElementById("myP").textContent = "i like briyani.";
+// document.getElementById("myP").textContent = "i like briyani.";
 
 
 
 // varaiable in javascript
+
+
+// let x  = 12;
+//  x = 34;
+
+//  console.log(x);
+
+// let age = 25;
+// console.log(age);
+// console.log(`you are ${age} years old`);
+
+// console.log(typeof age);
+
+
+
+
+
+//-------how to accept the user input
+
+// const username =window.prompt("Enter the username");
+
+// console.log(username);
+
+
+
+
+
+
+// let username;
+// document.getElementById("mysubmit").onclick = function(){
+//     username = document.getElementById("mytext").value;
+//    console.log(username);
+// }
+
+// document.getElementById("mysubmit").onclick = function(){
+//     const  title = document.getElementById("mytext").value;
+//     document.getElementById("myH1").textContent = `hello this is ${title}`
+// }
+
