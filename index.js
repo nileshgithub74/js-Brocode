31f48775a6a6edda695c74b729d40173d2bc2816
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-08-15 05:37:01 +0000

    js

--- index.js
+++ index.js
@@ -351,32 +351,32 @@ this is multi- line comment
 /////------------- Temperature Convertor-----------
 
 
-const textbox = document.getElementById("textbox");
-const tofahrenheit = document.getElementById("tofahrenheit");
-const toCelsius = document.getElementById("toCelsius");
+// const textbox = document.getElementById("textbox");
+// const tofahrenheit = document.getElementById("tofahrenheit");
+// const toCelsius = document.getElementById("toCelsius");
 
-const result = document.getElementById("result");
-let temp;
+// const result = document.getElementById("result");
+// let temp;
 
 
-function convert(){
-    if(tofahrenheit.checked){
-        temp = Number(textbox.value);
-        temp = temp *   9/5+32;
-        result.textContent=temp.toFixed(2) +   "°F";
+// function convert(){
+//     if(tofahrenheit.checked){
+//         temp = Number(textbox.value);
+//         temp = temp *   9/5+32;
+//         result.textContent=temp.toFixed(2) +   "°F";
         
 
 
-    }else if(toCelsius.checked){
-        temp = Number(textbox.value);
-        temp = (temp -32) * (5/9);
-        result.textContent=temp.toFixed(2) +   "°C";
+//     }else if(toCelsius.checked){
+//         temp = Number(textbox.value);
+//         temp = (temp -32) * (5/9);
+//         result.textContent=temp.toFixed(2) +   "°C";
 
 
-    }else{
-        result.textContent=`select the unit`;
-    }
+//     }else{
+//         result.textContent=`select the unit`;
+//     }
 
 
-}
+// }
 
