27721fb0738f4b51dd357b0a5b3d7efc35128fd3
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-08-18 12:44:10 +0000

    js

---
+++ classes.js
@@ -0,0 +1,15 @@
+// class  Product{
+//     constructor(names,price){
+//         this.names = names;
+//         this.price =price;
+//     }
+
+//     displayProduct(){
+//         console.log(`product : ${this.names}`);
+//         console.log(`price : ${this.price}`);
+//     }
+// }
+
+// const pro1 = new Product("Shirt",199);
+// pro1.displayProduct();
+
