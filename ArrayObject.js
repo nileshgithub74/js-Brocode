1c877d1554d085a27153f374b6b61ceef9af5730
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-08-18 19:37:08 +0000

    js

---
+++ ArrayObject.js
@@ -0,0 +1,21 @@
+const fruits = [
+    {name : "apple", color : " red", calcories : 34},
+    {name : "Orange", color : " Orange", calcories : 334},
+    {name : "cocuonut", color : " white", calcories : 24},
+
+
+];
+
+
+// console.log(fruits[0].name);
+
+
+// fruits.forEach(display);
+// function display(fruits){
+//     console.log(fruits);
+// }
+
+
+fruits.forEach( fruits =>{
+    console.log(fruits);
+})
---
+++ nestedObject.js
@@ -0,0 +1,33 @@
+// const person={
+//     fullName : "Nilesh kumar",
+//     age : 21,
+//     isStudent : true,
+
+//     address :{
+//         street : "phagwara",
+//         city : "jalandhar",
+//         country : " Punjab",
+//     }
+// }
+
+// console.log(person.address.street);
+
+
+
+
+
+class Person{
+    constructor(name, age, ...address){
+        this.name = name;
+        this.age = age;
+        this.address = new Adress(...address);
+
+    }
+}
+class Adress{
+    constructor(street, country,city){
+        this.country = country;
+        this.street = street;
+        this.city = city;
+    }
+}
