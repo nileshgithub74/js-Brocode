d3940e54d4b961c08db59e12f0831f9646a1f3e1
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-08-19 03:56:01 +0000

    js

---
+++ SetTimout.js
@@ -0,0 +1,17 @@
+// function sayHello(){
+//     console.log("Hiii, Good Morning");
+// }
+
+
+// setTimeout(sayHello,3000);
+
+
+// setTimeout(function(){
+//     console.log("hello world");
+// },3000);
+
+
+
+setTimeout(()=>{
+    console.log("hello world");
+},3000);
