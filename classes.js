5ddd849bff9d6614bce958a3213f953976f49e9b
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-08-23 10:55:12 +0000

    js

--- classes.js
+++ classes.js
@@ -28,3 +28,30 @@
 // console.log(sb.name);
 // sb.display();
 
+
+
+
+class SmartPhone{
+    constructor(name, price){
+        this.name = name;
+        this.price = price;
+    }
+    display(){
+        console.log(`the price of Iphone is ${this.price}`);
+    }
+}
+
+
+class series extends SmartPhone{
+    constructor(model,name,price){
+        super(name,price);
+        this.model = model;
+    }
+    display(){
+        console.log(`Model Name : ${this.model}`);
+    }
+
+}
+
+const model = new series("15 pro");
+model.display();
