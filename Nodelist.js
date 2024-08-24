c1369999da4060408005f3088ea0fa8e78224abd
Nilesh kumar <117479640+nileshgithub74@users.noreply.github.com>
2024-08-24 05:17:53 +0000

    js

---
+++ Nodelist.js
@@ -0,0 +1,2 @@
+// Node list   = static collection of hmtl eleement by(id,class, elemeent)  can be created by using querySelectorAll()   similar to array , but(map filter , reduce)   forEach method have
+// Nodelist wont update to automatically reflect changes
